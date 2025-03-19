5652ab93af0f95c2c8ab6f87f999f277ff797850
BalaArthi <balaarthimail@gmail.com>
2025-03-19 19:10:56 +0530

    Add files via upload

---
+++ TUPLE.py
@@ -0,0 +1,11 @@
+
+# getting user input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+gender = input("Enter your gender: ")
+city = input("Enter your city: ")
+japanese_level = input("Enter your Japanese level: ")
+
+# Creating a tuple 
+user_data = (name, age, gender, city, japanese_level)
+print(user_data)
